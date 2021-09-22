a46c267b284dbceeaba5eda7084167099a01d3d4
maxwellbenton <maxwellbenton@gmail.com>
2021-09-21 23:17:39 -0400

    initial build

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <SplashPage msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import SplashPage from './components/SplashPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SplashPage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/SplashPage.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="splash">
+    <div class="central">
+      <h1>Evan Hood Benton</h1>
+      <div class="links">
+        <a target="_blank" href="https://www.youtube.com/user/CrystalSkillet/featured">
+          <img id="youtube" alt="youtube" src="../assets/youtube.png"/>
+        </a>
+        <a target="_blank" href="https://www.instagram.com/evanhoodbenton/">
+          <img id="instagram" alt="instagram" src="../assets/instagram.png"/>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.splash {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+}
+
+.central {
+  width: 25vw;
+  min-width: 300px;
+}
+
+.links {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+}
+
+#youtube {
+  height: 50px;
+}
+
+#instagram {
+  height: 35px;
+}
+</style>
